692dba42223a5cd65f50a946c5c2a9929bbcee51
Eli Selkin <ejselkin@cpp.edu>
2017-06-11 20:19:46 -0700

    Got access token and added login screen... now need to pass that back to creating a version of Home with an access token in the props.

--- Login.js
+++ Login.js
@@ -11,35 +11,88 @@ const styles = StyleSheet.create({
         bottom: 0,
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center'
     },
     loginbox: {
-        marginLeft: 30,
+        marginTop: 10,
         padding: 10,
         width: 300,
         height: 50,
         borderColor: 'gray',
-        borderWidth: 1
-    }
+        borderWidth: 1,
+        borderRadius: 5,
 
+    },
+    buttonlogin: {
+        fontSize: 20,
+        backgroundColor: 'green',
+        padding: 20,
+        borderRadius: 5
+    },
+    loginholder: {
+        height: 200,
+    },
+    loginimageview: {
+        marginTop: 200,
+        alignItems: 'center',
+    },
+    loginimage: {
+        height:150,
+        width: 150
+    }
 });
 
 
 class Login extends Component {
-    getToken = () => {
+    doLogin = () => {
         fetch('https://curbmap.com/token')
-            .then((response) => response.text()).then((responseText) => { this.setState({XSRF: responseText}) })
+            .then((response) => response.text())
+            .then((responseText) => {
+                this.setState({XSRF: responseText})
+            })
+            .then(() => {
+                fetch('https://curbmap.com/login', {
+                    method: 'post',
+                    mode: 'cors',
+                    headers: {
+                        "X-XSRF-TOKEN": this.state.XSRF,
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: "username="+this.state.user+"&password="+this.state.pass
+                }).then((responseLogin) => {
+                    fetch('https://curbmap.com/user', {
+                        method: 'get',
+                        mode: 'cors',
+                        headers: {
+                            "X-XSRF-TOKEN": this.state.XSRF
+                        }
+                    })
+                        .then((responseUser) => responseUser.json())
+                        .then((responseUserJSON) => {
+                            fetch('https://curbmap.com/oauth/token', {
+                                method: 'post',
+                                headers: {
+                                    "Content-Type": "application/x-www-form-urlencoded",
+                                    "Authorization": "Basic YW5kcm9pZDFTOVdnYU05bHJJNnVqcXo0NDZ2Q3JQUktJNHA1MXFQOnJWZUI4NWhXVDN0REkxYU5LSVhiOTlyTTRLNDN0ME1B"
+                                },
+                                body: "username="+this.state.user+"&password="+this.state.pass+"&grant_type=password"
+                            })
+                                .then((oauthToken)=> oauthToken.json())
+                                .then((oauthTokenJSON) => {
+                                    console.log(oauthTokenJSON);
+                                })
+                        })
+                        .catch((e) => {console.log("Error in login: "+ e)})
+                })
+            })
     };
-    constructor(props) {
-        super(props);
-        this.XSRFTOKEN = null;
+
+    constructor(props, context) {
+        super(props, context);
         this.state = {};
     }
 
     componentDidMount() {
-        const value = this.getToken();
-        console.log("XSRF:" + this.state.XSRF);
     }
 
     static navigationOptions = {
@@ -52,7 +105,7 @@ class Login extends Component {
     };
 
     _submit = () => {
-
+        this.doLogin();
     };
 
     render() {
@@ -60,18 +113,40 @@ class Login extends Component {
             <View style={styles.full}>
                 <MenuIcon onPress={() => this.props.navigation.navigate('DrawerOpen')} />
 
-                <TextInput style={styles.loginbox}
-                           onChangeText={(text) => this.setState({user: text})}
-                           placeholder="username"
-                           value={this.state.user} />
+                <View style={styles.loginholder}>
+                    <View style={styles.loginimageview}>
+                        <Image
+                            style={styles.loginimage}
+                            source={require('./assets/img/curbmap.png')}
+                        />
+                    </View>
+                    <TextInput
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        style={styles.loginbox}
+                        onChangeText={(text) => this.setState({user: text})}
+                        placeholder='username'
+                        placeholderTextColor='lightgray'
+                        value={this.state.user} />
 
+                    <TextInput
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        style={styles.loginbox}
+                        onChangeText={(text) => this.setState({pass: text})}
+                        placeholder="password"
+                        placeholderTextColor='lightgray'
+                        secureTextEntry={true}
+                        value={this.state.pass} />
 
-                <TextInput style={styles.loginbox}
-                           onChangeText={(text) => this.setState({pass: text})}
-                           placeholder="password"
-                           value={this.state.pass} />
-                
-                <Button title="Login" onPress={this._submit} title="Login" style={{background: 'darkslategray', position: 'absolute', left: 30, top: 300, width:200, height: 100}}/>
+                    <Button
+                        title="Login"
+                        onPress={() => this._submit()}
+                        color="#841584"
+                        style={{fontSize: 20, color: 'green'}}>
+                        Login
+                    </Button>
+                </View>
             </View>
         )
     }
