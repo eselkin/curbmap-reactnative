bf93fb259965066f62a5957e8325f422cc8d9772
Eli Selkin <ejselkin@cpp.edu>
2017-06-11 22:12:20 -0700

    clean up display

--- Login.js
+++ Login.js
@@ -1,155 +1,161 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Image, TextInput, Button } from 'react-native'
+import { StyleSheet, View, Image, TextInput, Button, Dimensions } from 'react-native'
 import MenuIcon from './MenuIcon'
 
-const styles = StyleSheet.create({
-    full: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center'
-    },
-    loginbox: {
-        marginTop: 10,
-        padding: 10,
-        width: 300,
-        height: 50,
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 5,
+const {width, height} = Dimensions.get("window");
 
-    },
-    buttonlogin: {
-        fontSize: 20,
-        backgroundColor: 'green',
-        padding: 20,
-        borderRadius: 5
-    },
-    loginholder: {
-        height: 200,
-    },
-    loginimageview: {
-        marginTop: 200,
-        alignItems: 'center',
-    },
-    loginimage: {
-        height:150,
-        width: 150
-    }
+
+const styles = StyleSheet.create({
+  full: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  loginbox: {
+    marginTop: 10,
+    padding: 10,
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+
+  },
+  buttonlogin: {
+    fontSize: 20,
+    backgroundColor: 'green',
+    padding: 20,
+    borderRadius: 5
+  },
+  loginholder: {
+    flexDirection: 'column',
+    marginTop: height * 0.10,
+    padding: 10,
+    height: height * 0.7,
+    width: width * 0.8
+  },
+  loginimageview: {
+    alignItems: 'center',
+  },
+  loginimage: {
+    height:150,
+    width: 150
+  }
 });
 
 
 class Login extends Component {
-    doLogin = () => {
-        fetch('https://curbmap.com/token')
-            .then((response) => response.text())
-            .then((responseText) => {
-                this.setState({XSRF: responseText})
-            })
-            .then(() => {
-                fetch('https://curbmap.com/login', {
-                    method: 'post',
-                    mode: 'cors',
-                    headers: {
-                        "X-XSRF-TOKEN": this.state.XSRF,
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    body: "username="+this.state.user+"&password="+this.state.pass
-                }).then((responseLogin) => {
-                    fetch('https://curbmap.com/user', {
-                        method: 'get',
-                        mode: 'cors',
-                        headers: {
-                            "X-XSRF-TOKEN": this.state.XSRF
-                        }
-                    })
-                        .then((responseUser) => responseUser.json())
-                        .then((responseUserJSON) => {
-                            fetch('https://curbmap.com/oauth/token', {
-                                method: 'post',
-                                headers: {
-                                    "Content-Type": "application/x-www-form-urlencoded",
-                                    "Authorization": "Basic YW5kcm9pZDFTOVdnYU05bHJJNnVqcXo0NDZ2Q3JQUktJNHA1MXFQOnJWZUI4NWhXVDN0REkxYU5LSVhiOTlyTTRLNDN0ME1B"
-                                },
-                                body: "username="+this.state.user+"&password="+this.state.pass+"&grant_type=password"
-                            })
-                                .then((oauthToken)=> oauthToken.json())
-                                .then((oauthTokenJSON) => {
-                                    console.log(oauthTokenJSON);
-                                })
-                        })
-                        .catch((e) => {console.log("Error in login: "+ e)})
+  doLogin = () => {
+    fetch('https://curbmap.com/token')
+        .then((response) => response.text())
+        .then((responseText) => {
+          this.setState({XSRF: responseText})
+        })
+        .then(() => {
+          fetch('https://curbmap.com/login', {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+              "X-XSRF-TOKEN": this.state.XSRF,
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "username="+this.state.user+"&password="+this.state.pass
+          })
+              .then((responseLogin) => {
+                fetch('https://curbmap.com/user', {
+                  method: 'get',
+                  mode: 'cors',
+                  headers: {
+                    "X-XSRF-TOKEN": this.state.XSRF
+                  }
                 })
-            })
-    };
+                    .then((responseUser) => responseUser.json())
+                    .then((responseUserJSON) => {
+                      fetch('https://curbmap.com/oauth/token', {
+                        method: 'post',
+                        headers: {
+                          "Content-Type": "application/x-www-form-urlencoded",
+                          "Authorization": "Basic YW5kcm9pZDFTOVdnYU05bHJJNnVqcXo0NDZ2Q3JQUktJNHA1MXFQOnJWZUI4NWhXVDN0REkxYU5LSVhiOTlyTTRLNDN0ME1B"
+                        },
+                        body: "username="+this.state.user+"&password="+this.state.pass+"&grant_type=password"
+                      })
+                          .then((oauthToken)=> oauthToken.json())
+                          .then((oauthTokenJSON) => {
+                            console.log(oauthTokenJSON);
+                          })
+                    })
+                    .catch((e) => {console.log("Error in login: "+ e)})
+              })
+        })
+  };
 
-    constructor(props, context) {
-        super(props, context);
-        this.state = {};
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
 
-    componentDidMount() {
-    }
+  componentDidMount() {
+  }
 
-    static navigationOptions = {
-        drawerLabel: 'Login',
-        drawerIcon: ({tintColor}) => (
-            <Image
-                style={[styles.icon, {tintColor}]}
-            />
-        ),
-    };
+  static navigationOptions = {
+    drawerLabel: 'Login',
+    drawerIcon: ({tintColor}) => (
+        <Image
+            style={[styles.icon, {tintColor}]}
+        />
+    ),
+  };
 
-    _submit = () => {
-        this.doLogin();
-    };
+  _submit = () => {
+    this.doLogin();
+  };
 
-    render() {
-        return (
-            <View style={styles.full}>
-                <MenuIcon onPress={() => this.props.navigation.navigate('DrawerOpen')} />
+  render() {
+    return (
+        <View style={styles.full}>
+          <MenuIcon onPress={() => this.props.navigation.navigate('DrawerOpen')} />
 
-                <View style={styles.loginholder}>
-                    <View style={styles.loginimageview}>
-                        <Image
-                            style={styles.loginimage}
-                            source={require('./assets/img/curbmap.png')}
-                        />
-                    </View>
-                    <TextInput
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                        style={styles.loginbox}
-                        onChangeText={(text) => this.setState({user: text})}
-                        placeholder='username'
-                        placeholderTextColor='lightgray'
-                        value={this.state.user} />
-
-                    <TextInput
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                        style={styles.loginbox}
-                        onChangeText={(text) => this.setState({pass: text})}
-                        placeholder="password"
-                        placeholderTextColor='lightgray'
-                        secureTextEntry={true}
-                        value={this.state.pass} />
-
-                    <Button
-                        title="Login"
-                        onPress={() => this._submit()}
-                        color="#841584"
-                        style={{fontSize: 20, color: 'green'}}>
-                        Login
-                    </Button>
-                </View>
+          <View style={styles.loginholder}>
+            <View style={styles.loginimageview}>
+              <Image
+                  style={styles.loginimage}
+                  source={require('./assets/img/curbmap.png')}
+              />
             </View>
-        )
-    }
+            <TextInput
+                autoCorrect={false}
+                autoCapitalize='none'
+                style={styles.loginbox}
+                onChangeText={(text) => this.setState({user: text})}
+                placeholder='username'
+                placeholderTextColor='lightgray'
+                value={this.state.user} />
+
+            <TextInput
+                autoCorrect={false}
+                autoCapitalize='none'
+                style={styles.loginbox}
+                onChangeText={(text) => this.setState({pass: text})}
+                placeholder="password"
+                placeholderTextColor='lightgray'
+                secureTextEntry={true}
+                value={this.state.pass} />
+
+            <Button
+                title="Login"
+                onPress={() => this._submit()}
+                color="#841584"
+                style={{fontSize: 20, color: 'green'}}>
+              Login
+            </Button>
+          </View>
+        </View>
+    )
+  }
 }
 
 export default Login
--- Map.js
+++ Map.js
@@ -1,85 +1,88 @@
 import React, {
-    Component
+  Component
 } from 'react'
 import MapView from 'react-native-maps'
 import {
-    StyleSheet
+  StyleSheet
 } from 'react-native'
 import {
-    Permissions
+  Permissions
 } from 'expo';
 
 const styles = StyleSheet.create({
-    map: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-    },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
 const LATITUDE_DELTA = 0.1922;
 const LONGITUDE_DELTA = 0.1421;
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-        this.state = this.getInitialState();
-    }
+  constructor(props) {
+    super(props);
+    this.state = this.getInitialState();
+  }
 
-    _onRegionChange = (region) => {
-        this.setState({
-            region: region
-        });
-    };
+  _onRegionChange = (region) => {
+    this.setState({
+      region: region
+    });
+  };
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
-    componentDidMount() {
-        //https://stackoverflow.com/questions/38122649/how-to-create-maps-which-detect-automatic-location-in-react-native
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({
-                    region: {
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                        latitudeDelta: LATITUDE_DELTA,
-                        longitudeDelta: LONGITUDE_DELTA
-                    }
-                });
-            },
-            (error) => alert(error.message),
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-        );
-        this.watchID = navigator.geolocation.watchPosition((position) => {
-            const newRegion = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA
-            };
-
-            this._onRegionChange(newRegion);
-        });
-    }
-
-    getInitialState() {
-        return {
+  componentDidMount() {
+    //https://stackoverflow.com/questions/38122649/how-to-create-maps-which-detect-automatic-location-in-react-native
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
             region: {
-                latitude: 34.0928,
-                longitude: -118.3587,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA,
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA
             }
-        };
-    }
+          });
+        },
+        (error) => alert(error.message),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const newRegion = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      };
 
-    render() {
-        return ( <MapView style={ styles.map }
-                          region={ this.state.region }
-                          onRegionChange={ this._onRegionChange }
-                          loadingEnabled
-                          showsUserLocation/>
-        )
-    }
+      this._onRegionChange(newRegion);
+    });
+  }
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 34.0928,
+        longitude: -118.3587,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
+
+  render() {
+    return ( <MapView style={ styles.map }
+                      region={ this.state.region }
+                      onRegionChange={ this._onRegionChange }
+                      loadingEnabled
+                      showsUserLocation/>
+    )
+  }
 }
 
 export default Map
