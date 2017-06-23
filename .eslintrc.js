f970ca35e45b666cf6d604a82ad3bf93f9b0fcdd
Tommy Kelly <tommy.kelly@comcast.com>
2017-06-23 02:25:03 -0700

    add prepush lint hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: 'airbnb',
-  parser: 'babel-eslint', // needed for decorators and class properties
+  parser: 'babel-eslint', // needed for class properties
   rules: {
-    'linebreak-style': ['error', 'unix'],
     semi: ['error', 'never'],
-    // Avoid code that looks like two expressions but is actually one
-    // Allows omission of semicolons
-    'no-unexpected-multiline': 2,
   },
 }
